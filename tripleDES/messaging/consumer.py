b36ad75249466b5e0a50fe5a205858cf485e3533
Trevillion <abotsistanley0@gmail.com>
2024-03-22 05:26:22 +0000

    django channels setup

--- tripleDES/messaging/consumer.py
+++ tripleDES/messaging/consumer.py
@@ -1,30 +1,34 @@
+import json
+
 from channels.generic.websocket import AsyncWebsocketConsumer
 
+
 class MessagingConsumer(AsyncWebsocketConsumer):
-    groups = ["broadcast"]
-
     async def connect(self):
-        # Called on connection.
-        # To accept the connection call:
-        await self.accept()
-        # Or accept the connection and specify a chosen subprotocol.
-        # A list of subprotocols specified by the connecting client
-        # will be available in self.scope['subprotocols']
-        await self.accept("subprotocol")
-        # To reject the connection, call:
-        await self.close()
+        self.messaging_channel = "message_channel"
 
-    async def receive(self, text_data=None, bytes_data=None):
-        # Called with either text_data or bytes_data for each frame
-        # You can call:
-        await self.send(text_data="Hello world!")
-        # Or, to send a binary frame:
-        await self.send(bytes_data="Hello world!")
-        # Want to force-close the connection? Call:
-        await self.close()
-        # Or add a custom WebSocket error code!
-        await self.close(code=4123)
+        # Join room group
+        await self.channel_layer.group_add(self.messaging_channel, self.channel_name)
+
+        await self.accept()
 
     async def disconnect(self, close_code):
-        pass
-        # Called when the socket closes
+        # Leave room group
+        await self.channel_layer.group_discard(self.messaging_channel, self.channel_name)
+
+    # Receive message from WebSocket
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json["message"]
+
+        # Send message to room group
+        await self.channel_layer.group_send(
+            self.messaging_channel, {"type": "encrypted_message", "message": message}
+        )
+
+    # Receive message from room group
+    async def chat_message(self, event):
+        message = event["message"]
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({"message": message}))
--- tripleDES/tripleDES/asgi.py
+++ tripleDES/tripleDES/asgi.py
@@ -10,7 +10,18 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 import django
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tripleDES.settings')
 django.setup()
 application = get_asgi_application()
+
+import messaging.routing
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": URLRouter(messaging.routing.websocket_urlpatterns),
+    }
+)
