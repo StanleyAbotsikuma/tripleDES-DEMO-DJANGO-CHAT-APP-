25297e3e7613faa8f273797bfd04c3c6003ae5f6
Trevillion <abotsistanley0@gmail.com>
2024-03-23 10:52:27 +0000

    consumer resposive

--- tripleDES/messaging/views.py
+++ tripleDES/messaging/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 # Create your views here
 def single_page(request):
     return render(request,'single_page.html')
+
+
+
 def single_page1(request):
     return render(request,'single_page1.html')
